169199721417fdc3415aa1e9f01f77ef0d134949
Suyoung <msuyoung5@gmail.com>
2024-02-20 10:24:01 +0800

    Retrieved people from model and displayed name, subject_list on splash.html

--- GISFirstDjango/views.py
+++ GISFirstDjango/views.py
@@ -3,6 +3,7 @@ from .models import Person
 
 def splash(request):
     name = 'Billy'
+    subject_list = ['Chemistry', 'Mathematics', 'Economics']
     people = Person.objects.all()
     debug_people = list(people)
-    return render(request, "splash.html", {'name': name})
+    return render(request, "splash.html", {'name': name, 'subject_list': subject_list, 'people': debug_people})
